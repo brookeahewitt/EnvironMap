4d81ad41a1b840b7249d244e8c8ec0079409ea73
Ashley Whitehouse <uku9qn@virginia.edu>
2023-04-15 15:44:26 -0400

    changed buttons

--- main.py
+++ main.py
@@ -59,9 +59,9 @@ search_light = ImageTk.PhotoImage(Image.open('search_light.png').resize((70,70),
 
 get_location_button = ctk.CTkButton(master=root, width=80, height=32, fg_color=('#80ed99', '#57CC99'), hover_color=('#80ed99', '#57CC99'), border_width=0, corner_radius=8, image=search_light,
                                     text='', command=get_location)
-get_location_button.place(relx=0.68, rely=0.05, anchor=ctk.CENTER)
+get_location_button.place(relx=0.58, rely=0.05, anchor=ctk.CENTER)
 
-rightSide = ctk.CTkFrame(master=root, height=1200, width=300, fg_color='light blue')
+rightSide = ctk.CTkFrame(master=root, height=1200, width=300, fg_color=('#4DE3E8', '#38A3A5'))
 rightSide.place(relx=1, rely=0.5, anchor=ctk.E)
 
 places_label = ctk.CTkLabel(master=root, text="Places", width=120, height=25, fg_color=("white", "gray75"), corner_radius=8)
@@ -73,34 +73,6 @@ print(coords)
 markers = []
 
 # CODE TO PUT IN BUTTON --------
-
-print(locations)
-markers = []
-global right_labels
-right_labels = []
-map_widget.delete_all_marker()
-
-for i in range(len(right_labels)):
-    right_labels[i].destroy()
-
-y_val = 0.1
-
-for i in range(len(locations)):
-    if (y_val > 1):
-        break
-    location_text = locations[i][0] + "\n" + locations[i][1]
-    label = ctk.CTkLabel(master=root, text=location_text, width=120, height=25, fg_color=("white", "gray75"), corner_radius=8)
-    label.place(relx=0.9, rely=y_val, anchor=ctk.CENTER)
-    right_labels.append(label)
-    y_val += 0.05
-
-for i in range(len(locations)):
-    marker = "marker" + str(i)
-    markers.append(marker)
-
-for i in range(len(locations)):
-    markers[i] = map_widget.set_position(locations[i][2], locations[i][3], marker=True)
-    markers[i].set_text(locations[i][0])
 
 # set current widget position by address
 map_widget.set_position(latitude, longitude)
@@ -299,10 +271,12 @@ logo_light = ImageTk.PhotoImage(Image.open('logo_light.png'),Image.ANTIALIAS)
 logo_label = ctk.CTkLabel(master=root, image=logo_light, text="")
 logo_label.place(relx=0.5, rely=0.95, anchor=ctk.CENTER)
 
-back_food_b = ctk.CTkButton(foodSubframe, fg_color='orange', command=backFoodButton)
-back_shopping_b = ctk.CTkButton(shoppingSubframe, fg_color='orange', command=backShoppingButton)
-back_transportation_b = ctk.CTkButton(transportationSubframe, fg_color='orange', command=backTransportationButton)
-back_nature_b = ctk.CTkButton(natureSubframe, fg_color='orange', command=backNatureButton)
+back_light = ImageTk.PhotoImage(Image.open('back_light.png').resize((120,120),Image.ANTIALIAS))
+
+back_food_b = ctk.CTkButton(foodSubframe, fg_color=('#80ed99', '#57CC99'), image=back_light, command=backFoodButton)
+back_shopping_b = ctk.CTkButton(shoppingSubframe, fg_color=('#80ed99', '#57CC99'), image=back_light, command=backShoppingButton)
+back_transportation_b = ctk.CTkButton(transportationSubframe, fg_color=('#80ed99', '#57CC99'), image=back_light, command=backTransportationButton)
+back_nature_b = ctk.CTkButton(natureSubframe, fg_color=('#80ed99', '#57CC99'), image=back_light, command=backNatureButton)
 
 
 # Put them on the frame
